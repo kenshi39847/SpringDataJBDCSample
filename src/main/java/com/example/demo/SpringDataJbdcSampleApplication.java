4a7b374bfe0e419788f1de99ee7d9a37cf3bcccc
kenshi39847 <kenshi.t.blue@gmail.com>
2024-01-06 14:24:29 +0900

    「使う側」クラスの作成

--- src/main/java/com/example/demo/SpringDataJbdcSampleApplication.java
+++ src/main/java/com/example/demo/SpringDataJbdcSampleApplication.java
@@ -1,13 +1,48 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.entity.Member;
+import com.example.demo.repository.MemberCrudRepository;
+
+// SpringBoot起動クラス
 @SpringBootApplication
 public class SpringDataJbdcSampleApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDataJbdcSampleApplication.class, args);
+		SpringApplication.run(SpringDataJbdcSampleApplication.class, args).getBean(SpringDataJbdcSampleApplication.class).execute();
+	}
+	
+	@Autowired
+	MemberCrudRepository repository;
+	
+	// 「登録」と「全権取得」を実行
+	private void execute() {
+		// 登録
+		executeInsert();
+		// 全件取得
+		executeSelect();
+	}
+	
+	// 登録
+	private void executeInsert() {
+		// エンティティの作成（idは自動連番で設定するためにnullを設定）
+		Member member = new Member(null, "花子");
+		// リポジトリを使用して登録を実施、結果を取得
+		member = repository.save(member);
+		// 結果を表示
+		System.out.println("登録したデータ：" + member);
+	}
+	
+	// 全件取得
+	private void executeSelect() {
+		System.out.println("---全件取得します---");
+		// リポジトリを使用して全件取得を実施、結果を取得
+		Iterable<Member> members = repository.findAll();
+		for (Member member : members) {
+			System.out.println(member);
+		}
 	}
 
 }
